6c66dc8468fc341596117cf7b4ca7f1c69a1e9e9
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2019-02-28 16:45:53 -0400

    Nasa deploy

---
+++ public/precache-manifest.314b0da2c029823e6d63ce4f79fe6f4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "61236a7bf5152e4c598f6060ff4244c4",
+    "url": "/static/media/spacex2.61236a7b.png"
+  },
+  {
+    "revision": "8dda6a9a16b06a7d76129dfb421b5de6",
+    "url": "/static/media/loader2.8dda6a9a.gif"
+  },
+  {
+    "revision": "7e2ad1b248139856e0e9f26eb480ddce",
+    "url": "/static/media/loader.7e2ad1b2.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3d7e7662f4757e9c2a0",
+    "url": "/static/js/main.e3d7e766.chunk.js"
+  },
+  {
+    "revision": "92fa3995488302512c00",
+    "url": "/static/js/1.92fa3995.chunk.js"
+  },
+  {
+    "revision": "e3d7e7662f4757e9c2a0",
+    "url": "/static/css/main.2b87415b.chunk.css"
+  },
+  {
+    "revision": "18c54d4605e0dae97c0c9eda5f6206cb",
+    "url": "/index.html"
+  }
+];
